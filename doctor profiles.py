f41a84d30c17f44f85033fef370aad59c8830813
Prakhar00001 <prakharsaini786@gmail.com>
2024-05-19 16:11:45 +0530

    Update doctor profiles.py

--- doctor profiles.py
+++ doctor profiles.py
@@ -7,6 +7,6 @@ def analyze_profile(profile):
   entities = [(X.text, X.label_) for X in doc.ents]
   return entities
 
-profile = "Dr. Anil Saini  is a general practitioner with 10 years of experience"
+profile = "Dr. Anil Saini  is the best cardiologist with 10 years of experience"
 entities = analyze_profile(profile)
 print(entities)
